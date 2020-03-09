8b5b38eacb700f983c6306bc8011679ac1e394ae
danesn <danesnovigar@gmail.com>
2020-03-09 18:42:24 +0700

    fitur hapus bisa dijalankan

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 let tombolsLihat = document.querySelectorAll(".tombol-lihat");
 let tombolsSilang = document.querySelectorAll(".silang");
+let tombolsHapus = document.querySelectorAll(".tombol-hapus");
 let modals = document.querySelectorAll(".modal");
 
 tombolsLihat.forEach(function(tombolLihat){
     tombolLihat.addEventListener('click', function(e){
         e.preventDefault();
-        tombolLihat.nextElementSibling.classList.add("modal-active");
+        this.nextElementSibling.classList.add("modal-active");
     })
 })
 
 tombolsSilang.forEach(function(tombolSilang){
     tombolSilang.addEventListener('click', function(e){
         e.preventDefault();
-        tombolSilang.parentElement.parentElement.classList.remove("modal-active");
+        this.parentElement.parentElement.classList.remove("modal-active");
+        e.stopPropagation();
+    })
+})
+
+tombolsHapus.forEach(function(tombolHapus){
+    tombolHapus.addEventListener('click', function(e){
+        e.preventDefault();
+        this.parentElement.parentElement.parentElement.remove();
         e.stopPropagation();
     })
 })
